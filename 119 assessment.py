4e6075de4d013085554616b4cfedc2cc78020a61
Nox-Donley <145069348+Nox-Donley@users.noreply.github.com>
2024-01-19 14:20:53 -0700

    Add files via upload

---
+++ 119 assessment.py
@@ -0,0 +1,12 @@
+choice = input("do you like winter or summer more?")
+choice2 = input("do you like sports or relaxing more?")
+if choice == "winter":
+    if choice2 == "sports":
+        print ("You should try skiing!!!")
+    elif choice2 == "relaxing":
+        print("you should try snuggling up and watching a movie!!!")
+elif choice == "summer":
+    if choice2 == "sports":
+        print ("You should try Jogging!!!")
+    elif choice2 == "relaxing":
+        print("you should try relaxing outside with your friends!!!")
